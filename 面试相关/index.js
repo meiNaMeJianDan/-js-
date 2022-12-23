77220d3b56ce874584f8dcb4980cc8bcd9eed6f3
luopeng <1469104767@qq.com>
2022-12-23 18:46:34 +0800

    增加面试题

--- 面试相关/index.js
+++ 面试相关/index.js
@@ -1,7 +1,7 @@
 // 中划线改小驼峰
-// const foo = (str)=>str?.replace(/-[a-z]/g, (match)=>match.replace('-', '').toUpperCase())
+let foo = (str)=>str?.replace(/-[a-z]/g, (match)=>match.replace('-', '').toUpperCase())
 // 小驼峰改中划线
-// const foo = (str)=>str?.replace(/[A-Z]/g,(a)=>"-"+a?.toLowerCase())
+foo = (str)=>str?.replace(/[A-Z]/g,(a)=>"-"+a?.toLowerCase())
 
 // 前端性能优化
 // 1.页面渲染：避免css,js渲染阻塞，降低css选择器复杂度，减少重绘和回流。
@@ -28,3 +28,58 @@
 
 // 客服端如何和服务端建立链接
 // 1：三次握手：a.客服端发送连接请求，b.服务器收到连接请求回复，返回消息给客服端。c.客服端想服务端发送同意连接请求 
+
+// 防抖与节流
+// 防抖：在n秒之后在发起一次回调，n秒之间再次触发都会重新计时
+// 节流：n秒之中只会触发一次回调。
+// //防抖
+let debounce = (fn, date)=> {
+    let timer //声明接收定时器的变量
+    return function (...arg) { // 获取参数
+        timer && clearTimeout(timer) // 清空定时器
+        timer = setTimeout(() => { //  生成新的定时器
+            //因为箭头函数里的this指向上层作用域的this,所以这里可以直接用this，不需要声明其他的变量来接收
+            fn.apply(this, arg) // fn()
+        }, date)
+    }
+}
+//--------------------------------
+// 节流
+debounce = (fn, date)=> {
+    let timer = +new Date() // 声明初始时间
+    return function (...arg) { // 获取参数
+        let newTimer = +new Date() // 获取触发事件的时间
+        if (newTimer - timer >= date) { // 时间判断,是否满足条件
+            fn.apply(this, arg) // 调用需要执行的函数,修改this值,并且传入参数
+            timer = +new Date() // 重置初始时间
+        }
+    }
+}
+
+// 什么是webpack
+// webpack是静态模块打包工具，主要作用是进行代码的分析，压缩，打包，其中主要参数有入口entry,输出output,加载器loader,插件plugin,模式mode
+
+// 创建对象的方法
+// 1.Object构造函数创建
+const a = new Object()
+// 2.Object的create方法
+const b = Object.create(null)
+// 3.字面量的方式
+const c = {}
+// 4.函数构造器
+function Person(name){
+    let obj = {}
+    obj.name = name
+    return obj
+}
+const d = new Person("hello")
+// 5.带有原型函数的构造函数
+Person.prototype.name = "hahaha"
+const e = new Person()
+// 6.ES6语法
+class Car{
+    constructor(name){
+        this.name = name
+    }
+}
+const f = new Car("benci")
